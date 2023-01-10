71779093a8688bac5a8002d1e38897cff2324ee9
Kamil Mensik <kamil.mensik@juicymo.cz>
2023-01-10 19:50:30 +0100

    Add smth

--- bullets.py
+++ bullets.py
@@ -8,13 +8,14 @@ def getBullets():
     return bullets
 
 class Bullet(pygame.sprite.Sprite):
-    def __init__(self, angle, speed, position, radius = 8) -> None:
+    def __init__(self, angle, speed, position, radius = 8, hitbox = 'circle') -> None:
         pygame.sprite.Sprite.__init__(self)
         self.image = pygame.image.load('sprites/bullet.png').convert_alpha()
         self.rect = self.image.get_rect(center=position)
         self.velocity = Vector2(0.1 * speed, 0).rotate(angle) * 5
         self.pos = Vector2(self.rect.center)
         self.radius = radius
+        self.hitbox = hitbox
         bullets.add(self)
     
     def update(self) -> None:
