fbaea77f8eb27f0d88afb131045b0896df81e2cb
Kamil Mensik <kamil.mensik@juicymo.cz>
2023-01-17 21:16:38 +0100

    Fix conflict

--- points.py
+++ points.py
@@ -12,10 +12,4 @@ class Point(pygame.sprite.Sprite):
     def update(self) -> None:
         if self.rect.x <= -50 or self.rect.x >= 1250 or self.rect.y <= -50 or self.rect.y >= 850:
             self.kill()
-<<<<<<< Updated upstream
         self.rect.y +=1.5
-=======
-        self.rect.y+=1
->>>>>>> Stashed changes
-
-
